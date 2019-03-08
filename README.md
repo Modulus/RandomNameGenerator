401c6e06b110ca6dcf9e847640544dad36e7fc4e
John Sigvald Skauge <johnsigvald.skauge@tv2.no>
2019-03-08 13:47:40 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# How
+pip install -r requirements.txt
+
+python main.py
+
--- main.py
+++ main.py
@@ -1,11 +1,33 @@
 import pandas
+import structlog
+
+logger = structlog.get_logger()
+
+
+animals_df = pandas.read_csv("./resources/animals.csv")
+
+animal_name_df = animals_df.sample()
+print(animal_name_df)
+animal_name = animal_name_df.values[0][0]
+
+logger.msg("animal name sampled", name=f"{animal_name}")
+
+
+adjectives_df = pandas.read_csv("./resources/adjectives.csv")
+adjective_df = adjectives_df.sample()
+
+adjective = adjective_df.values[0][0]
+
+logger.msg("adjective sampled", adjective=f"{adjective}")
+
+
+# Replace spaces with - in animal name
+animal_name = animal_name.replace(" ", "-")
+logger.msg(f"{adjective}{animal_name}")
 
 
 
-dataframe = pandas.read_csv("./resources/animals.csv")
-
-print(dataframe)
-print(dataframe.sample(n=1))
+print(f"{adjective}-{animal_name}")
 
 # import  csv
 #
